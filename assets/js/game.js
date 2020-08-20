dd3a66ba7e0000761f108a81fb452583e4654e4a
G.B.Abadio <gilabadio@gmail.com>
2020-08-19 22:00:22 -0400

    added random variables

--- assets/js/game.js
+++ assets/js/game.js
@@ -6,6 +6,7 @@ var playerMoney = 10;
 var enemyNames = ["Roborto", "Prince Robot IVV", "Robot Mike Tyson"];
 var enemyHealth = 50;
 var enemyAttack = 12;
+
 // you can also log multiple values at once like this
 
 var fight = function(enemyName) {
@@ -22,14 +23,15 @@ var fight = function(enemyName) {
           if (confirmSkip) {
             window.alert(playerName + ' has decided to skip this fight. Goodbye!');
             // subtract money from playerMoney for skipping
-            playerMoney = playerMoney - 10;
+            playerMoney = Math.max(0, playerMoney - 10);
             console.log("playerMoney", playerMoney)
             break;
           }
         }
     
         // remove enemy's health by subtracting the amount set in the playerAttack variable
-        enemyHealth = enemyHealth - playerAttack;
+        var damage = randomNumber(playerAttack - 3, playerAttack);
+        enemyHealth = Math.max(0, enemyHealth - damage);
         console.log(
           playerName + ' attacked ' + enemyName + '. ' + enemyName + ' now has ' + enemyHealth + ' health remaining.'
         );
@@ -48,7 +50,8 @@ var fight = function(enemyName) {
         }
     
         // remove players's health by subtracting the amount set in the enemyAttack variable
-        playerHealth = playerHealth - enemyAttack;
+        var damage = randomNumber(enemyAttack - 3, enemyAttack);
+        playerHealth = Math.max(0, playerHealth - damage);
         console.log(
           enemyName + ' attacked ' + playerName + '. ' + playerName + ' now has ' + playerHealth + ' health remaining.'
         );
@@ -77,7 +80,7 @@ var startGame = function(){
    
             var pickedEnemyName = enemyNames[i];
    
-            enemyHealth = 50;
+            enemyHealth = randomNumber(40, 60);
    
             fight(pickedEnemyName);
            
@@ -151,5 +154,10 @@ var shop = function(){
             shop();
             break;
     }
+};
+var randomNumber = function(min, max){
+    var value = Math.floor(Math.random() * (max - min + 1) + min);
+
+    return value;
 };
 startGame();
