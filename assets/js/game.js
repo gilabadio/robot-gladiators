4f2990098af191a1e8b5ae07b3d2e0293474402b
G.B.Abadio <gilabadio@gmail.com>
2020-08-19 21:30:32 -0400

    shop function finished

--- assets/js/game.js
+++ assets/js/game.js
@@ -80,6 +80,14 @@ var startGame = function(){
             enemyHealth = 50;
    
             fight(pickedEnemyName);
+           
+            if ( playerHealth > 0 && i< enemyNames.length - 1){
+                var storeConfirm = window.confirm("The fight is over, visit the store before the next round?");
+                if (storeConfirm) {
+                shop();
+            }
+
+            }
             }   
         else{
              window.alert("You have lost everything! Game Over!");
@@ -104,5 +112,44 @@ var endGame = function(){
         window.alert("Thank you for playing Robot Gladiators!");
     }
 };
-
+var shop = function(){
+    var shopOptionPrompt = window.prompt(
+        "would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please eneter one: 'REFILL', 'UPGRADE' or 'LEAVE' to make a choice."
+    );
+    switch(shopOptionPrompt){
+        case "refill":
+        case "REFILL":
+        case "Refill":
+            if(playerMoney >= 7){
+            window.alert("Refilling player's health by 20 for 7 dollars.");
+            playerHealth = playerHealth + 20;
+            playerMoney = playerMoney - 7;
+        }
+        else{
+            window.alert("You don't have enough money!");
+        }
+            break;
+        case "upgrade":
+        case "UPGRADE":
+        case "Upgrade":
+            if (playerMoney >= 7){
+            window.alert("Upgrading player's attack by 6 for 7 dollars.");
+            playerAttack = playerAttack + 6;
+            playerMoney = playerMoney - 7;
+            }
+            else{
+                window.alert("You don't have enough money")
+            }
+            break;
+        case "leave":
+        case "LEAVE":
+        case "Leave":
+            window.alert("Leave the store.");
+            break;
+        default:
+            window.alert("You did not pick a valid option. Try again.");
+            shop();
+            break;
+    }
+};
 startGame();
