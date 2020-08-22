756c20d4ae4e4dcfd613289d1d732cca50316d88
G.B.Abadio <gilabadio@gmail.com>
2020-08-22 14:23:13 -0400

    fixed issue with mixed casing with the Skip prompt

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,25 +1,10 @@
 
 var fight = function(enemy) {
     while (playerInfo.health > 0 && enemy.health > 0) {
-        // ask user if they'd liked to fight or run
-        var promptFight = window.prompt('Would you like FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
-    
-        // if user picks "skip" confirm and then stop the loop
-        if (promptFight === "skip" || promptFight === "SKIP") {
-          // confirm user wants to skip
-          var confirmSkip = window.confirm("Are you sure you'd like to quit?");
-    
-          // if yes (true), leave fight
-          if (confirmSkip) {
-            window.alert(playerInfo.name + ' has decided to skip this fight. Goodbye!');
-            // subtract money from playerInfo.money for skipping
-            playerInfo.money = Math.max(0, playerInfo.money - 10);
-            console.log("playerInfo.money", playerInfo.money)
-            break;
-          }
-        }
-    
-        // remove enemy's health by subtracting the amount set in the playerInfo.attack variable
+       if (fightOrSkip()) {
+         break;
+       };  
+
         var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
         enemy.health = Math.max(0, enemy.health - damage);
         console.log(
@@ -192,5 +177,30 @@ var enemyInfo = [
     attack: randomNumber(12,16)
   }
 ];
+var fightOrSkip = function() {
+  // ask user if they'd like to fight or skip using  function
+  var promptFight = window.prompt('Would you like FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
+     
+  if (promptFight === "" || promptFight === null){
+    window.alert("You need to provide a valid answer! Please try again.");
+    return fightOrSkip();
+  }
+
+  // if user picks "skip" confirm and then stop the loop
+   promptFight = promptFight.toLocaleLowerCase();
+  
+   if (promptFight === "skip") {
+    // confirm user wants to skip
+    var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+
+    // if yes (true), leave fight
+    if (confirmSkip) {
+      window.alert(playerInfo.name + " has decided to skip this fight. Goodbye!");
+      // subtract money from playerMoney for skipping
+      playerInfo.playerMoney = playerInfo.money - 10;
+      return true;
+    }
+  }
+}
 
 startGame();
